f84b97d0a4ed85d4fe11dc30641f53f49576fdae
Diaz <zly981026@outlook.com>
2018-09-26 14:44:08 +0800

    Update 1#：由一个中奖号码增加到四个

--- LuckyGoddess/src/edu/diaz/luckygoddess/LuckyTime.java
+++ LuckyGoddess/src/edu/diaz/luckygoddess/LuckyTime.java
@@ -1,5 +1,7 @@
 package edu.diaz.luckygoddess;
 
+import java.util.Arrays;
+
 /**
  * 用于实现抽奖功能的界面类
  * @author Diaz
@@ -9,7 +11,7 @@ public class LuckyTime implements LuckyGoddessUI
 {
 
 	public String uiName = "抽奖";
-	
+	private boolean isWinning = false;
 	@Override
 	/*
 	 * (non-Javadoc)
@@ -20,9 +22,13 @@ public class LuckyTime implements LuckyGoddessUI
 		// TODO Auto-generated method stub
 		System.out.println("欢迎你："+LuckyGoddess.user.getUserName());
 		System.out.println("您的会员ID是："+LuckyGoddess.userID);
-		String result = getLuckyNumber();
-		System.out.println("中奖号码为："+result);
-		System.out.println(result.equals(LuckyGoddess.userID)?"恭喜您中奖了！":"很遗憾您未中奖请再接再励！");
+		String[] result = getLuckyNumber();
+		System.out.println("中奖号码为："+Arrays.toString(result));
+		for (String string : result)
+		{
+			isWinning = string.equals(LuckyGoddess.userID)?true:false;
+		}
+		System.out.println(isWinning?"恭喜您中奖了！":"很遗憾您未中奖请再接再励！");
 		UserOptional.userOptional(LuckyGoddess.welcomeScreen);
 	}
 	
@@ -30,14 +36,19 @@ public class LuckyTime implements LuckyGoddessUI
 	 * 获取中奖号码
 	 * @return 中奖号码
 	 */
-	public String getLuckyNumber()
+	public String[] getLuckyNumber()
 	{
-		String luckyNumber="";
-		for(int i = 0;i<4;i++)
-		{
-			int temp = (int)((Math.random()*100)%10);
-			luckyNumber+=temp;
-		}
+		String[] luckyNumber=new String[5];
+	    for(int j =0;j<5;j++)
+	    {
+	    	String luckyNumberTemp = "";
+	    	for(int i = 0;i<4;i++)
+			{
+				int temp = (int)((Math.random()*100)%10);
+				luckyNumberTemp+=temp;
+			}
+	    	luckyNumber[j] = luckyNumberTemp;
+	    }
 		return luckyNumber;
 	}
 
