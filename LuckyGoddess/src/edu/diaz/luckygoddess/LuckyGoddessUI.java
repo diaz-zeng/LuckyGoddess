d528e33db25acf9a75fda4a9a1b95092c951d72c
Diaz <zly981026@outlook.com>
2018-09-26 20:09:56 +0800

    Final version

--- LuckyGoddess/src/edu/diaz/luckygoddess/LuckyGoddessUI.java
+++ LuckyGoddess/src/edu/diaz/luckygoddess/LuckyGoddessUI.java
@@ -1,12 +1,14 @@
 package edu.diaz.luckygoddess;
 
 /**
- * 界面接口，所有的界面需要实现这个接口 
+ * 界面接口，所有的界面需要实现这个接口
+ * 
  * @author Diaz
  *
  */
 public interface LuckyGoddessUI
 {
-	public void start();//启动页面的方法
-	public String uiName="";//页面的名称
+	public void start();// 启动页面的方法
+
+	public String uiName = "";// 页面的名称
 }
--- LuckyGoddess/src/edu/diaz/luckygoddess/WelcomeScreen.java
+++ LuckyGoddess/src/edu/diaz/luckygoddess/WelcomeScreen.java
@@ -1,58 +1,58 @@
 package edu.diaz.luckygoddess;
 
 import java.util.Scanner;
+
 /**
  * »¶Ó­½çÃæ
+ * 
  * @author Diaz
  *
  */
 public class WelcomeScreen implements LuckyGoddessUI
 {
-	
+
 	public String uiName = "»¶Ó­";
-	
-	
+
 	@Override
 	public void start()
 	{
 		// TODO Auto-generated constructor stub
-				System.out.println("****»¶Ó­½øÈë½±¿Í¸»ÎÌÏµÍ³****");
-				System.out.println("        1.×¢²á                           ");
-				System.out.println("        2.µÇÂ½                           ");
-				System.out.println("        3.³é½±                           ");
-				System.out.println("****************************");
-				System.out.print("ÇëÑ¡Ôò£º");
-				int userOption= new Scanner(System.in).nextInt();
-				switch (userOption)
-				{
-				case 1:
-				{
-			        
-			        UserOptional.userOptional(new RegistrationProcess());
-			        break;
-					
-				}
-				case 2:
-				{
-					
-					UserOptional.userOptional(new LoginProcess());
-					break;
-				}
-				case 3:
-				{
-					if(LuckyGoddess.isLogin)
-					{
-						UserOptional.userOptional(new LuckyTime());
-					}
-					else
-					{
-						UserOptional.userOptional(new LoginProcess());
-						System.out.println("ÄúÉÐÎ´µÄµÇÂ½£¬ÇëµÇÂ¼");
-					}
-					break;
-				}
-				default:
-					break;
-				}
+		System.out.println("****»¶Ó­½øÈë½±¿Í¸»ÎÌÏµÍ³****");
+		System.out.println("        1.×¢²á                           ");
+		System.out.println("        2.µÇÂ½                           ");
+		System.out.println("        3.³é½±                           ");
+		System.out.println("****************************");
+		System.out.print("ÇëÑ¡Ôò£º");
+		int userOption = new Scanner(System.in).nextInt();
+		switch (userOption)
+		{
+		case 1:
+		{
+
+			UserOptional.userOptional(new RegistrationProcess());
+			break;
+
+		}
+		case 2:
+		{
+
+			UserOptional.userOptional(new LoginProcess());
+			break;
+		}
+		case 3:
+		{
+			if (LuckyGoddess.isLogin)
+			{
+				UserOptional.userOptional(new LuckyTime());
+			} else
+			{
+				UserOptional.userOptional(new LoginProcess());
+				System.out.println("ÄúÉÐÎ´µÄµÇÂ½£¬ÇëµÇÂ¼");
+			}
+			break;
+		}
+		default:
+			break;
+		}
 	}
 }
